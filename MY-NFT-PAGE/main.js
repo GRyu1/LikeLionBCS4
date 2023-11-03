aaae4f815dc1afbb2d9168585bff988e340df3a6
GRyu1 <heliotrop1024@gmail.com>
2023-11-03 14:34:50 +0900

    Update main.js

--- MY-NFT-PAGE/main.js
+++ MY-NFT-PAGE/main.js
@@ -2,9 +2,9 @@ const clock = document.querySelector(".clock")
 
 const getTime = () => {    
     const date = new Date();
-    const sec = date.getSeconds();
-    const min = date.getMinutes();
-    const hour = date.getHours()%12;
+    const sec = date.getSeconds().toString().padStart(2,'0');
+    const min = date.getMinutes().toString().padStart(2,'0');
+    const hour = (date.getHours()>=12 && date.getHours()!==24 ? "PM " : "AM " )+ (date.getHours()%12).toString().padStart(2,'0');
 
     clock.innerText = `${hour}:${min}:${sec}`    
 };
