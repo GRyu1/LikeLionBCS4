060e6aaf6879471b31c112878fa10ce6e2cd964a
GRyu1 <heliotrop1024@gmail.com>
2023-11-07 17:16:33 +0900

    달력만들기 my-app

--- MY-NFT-PAGE/main.js
+++ MY-NFT-PAGE/main.js
@@ -59,3 +59,19 @@ function getQuotes() {
     quotes.style.display = "none";
     newQuotes.style.display = "block";
   }
+  async function getNft() {
+    const nftImg = document.querySelector(".nft-img");
+    const nftName = document.querySelector(".nft-name");
+    const nftDesc = document.querySelector(".nft-desc");
+  
+    const response = await axios.get(
+      "https://olbm.mypinata.cloud/ipfs/QmfLuVV5GdfjvzGgxJDU684B9tyhD4KY5vQD6ZwiAWBd5P"
+    );
+
+    nftImg.src = response.data.image
+    nftName.innerText = response.data.name;
+    nftDesc.innerText = response.data.description;
+    
+  }
+  
+  getNft();
---
+++ my-app/src/components/BoxA.jsx
@@ -0,0 +1,9 @@
+const BoxA = () => {
+    return (
+      <div className="bg-red-800 min-h-screen flex justify-center items-center gap-12">
+        Hello, React!
+      </div>
+    );
+  };
+  
+  export default BoxA;
